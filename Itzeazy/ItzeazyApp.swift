b3efadd6e34c0f2adfbbcf0457379608f690aa23
Nazmin Parween <nazminparween23@gmail.com>
2026-04-28 10:58:14 +0530

    Initial Commit

---
+++ Itzeazy/ItzeazyApp.swift
@@ -0,0 +1,17 @@
+//
+//  ItzeazyApp.swift
+//  Itzeazy
+//
+//  Created by Nazmin Parween on 28/04/26.
+//
+
+import SwiftUI
+
+@main
+struct ItzeazyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
